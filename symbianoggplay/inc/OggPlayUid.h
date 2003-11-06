669d480308b9f44c3db8ead5e4b7871a997ed680
bjerre <>
2003-11-06 21:59:23 +0000

    Exported UID file for OggPlay

---
+++ symbianoggplay/inc/OggPlayUid.h
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2003 L. H. Wilden.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+ // UIDs used for OggPlay
+
+
+#ifndef __OggPlayUid_h
+#define __OggPlayUid_h
+
+#if defined(__GCC32__) || defined(__VC32__)
+  #include <e32std.h>
+  static const TInt KOggPlayApplicationUidValue = 0x0BF72853;
+  static const TInt KOggPlayRecognizerUidValue  = 0x101F1234;
+  // Not sure this is correct mime.
+  #define KOggMimeType "application/ogg"
+#else
+  // For .AIF file inclusion
+  #define KOggPlayApplicationUidValue 0x0BF72853
+  #define KOggPlayRecognizerUidValue  0x101F1234
+#endif
+
+#endif
