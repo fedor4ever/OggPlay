4a8a1f8e9877fa57001f0717b1563715b2d9534f
lcanis <>
2003-11-30 20:36:22 +0000

    FullSteel skin

---
+++ symbianoggplay/bitmaps/Skins/FullSteel/FullSteel.bat
@@ -0,0 +1,5 @@
+call bmconv FullSteel.cmd
+call makesis FullSteel.pkg
+copy FullSteel.mbm \Epoc32\Release\wins\UDEB\Z\SYSTEM\apps\OGGPLAY
+copy FullSteel.skn \Epoc32\Release\wins\UDEB\Z\SYSTEM\apps\OGGPLAY
+pause
