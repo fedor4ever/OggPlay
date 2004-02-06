9e596a1192c33c5d879e9542fb760658e4fb8218
bjerre <>
2004-02-06 21:05:57 +0000

    Should not be here

--- symbianoggplay/bitmaps/Skins/GreyZone/AnalyzerTest.bat
+++
@@ -1,7 +0,0 @@
-call del *.mbm
-call del *.sis
-call bmconv AnalyzerTest.cmd
-call makesis AnalyzerTest.pkg
-copy *.mbm \epoc32\release\wins\udeb\z\system\apps\oggplay\
-copy *.skn \epoc32\release\wins\udeb\z\system\apps\oggplay\
-pause
