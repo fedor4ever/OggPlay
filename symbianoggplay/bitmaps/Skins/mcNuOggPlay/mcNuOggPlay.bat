3205321eee16d56b9ab9cf2c8d7ae7eed9ec709a
lcanis <>
2004-03-09 21:39:08 +0000

    Fundaluka skin

---
+++ symbianoggplay/bitmaps/Skins/mcNuOggPlay/mcNuOggPlay.bat
@@ -0,0 +1,7 @@
+rem call del *.mbm
+rem call del *.sis
+rem call bmconv Amber.cmd
+rem call makesis Amber.pkg
+copy *.mbm \epoc32\release\wins\udeb\z\system\apps\oggplay\
+copy *.skn \epoc32\release\wins\udeb\z\system\apps\oggplay\
+pause
