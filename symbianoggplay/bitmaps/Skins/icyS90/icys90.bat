3ad2c678a5cfbcecb1483d761df9475efd125dd4
fig7 <>
2006-03-25 16:40:43 +0000

    Added icyS90 skin

---
+++ symbianoggplay/bitmaps/Skins/icyS90/icys90.bat
@@ -0,0 +1,5 @@
+call bmconv icys90.cmd
+call makesis icys90.pkg
+copy icys90.mbm \Epoc32\Release\wins\UDEB\Z\SYSTEM\apps\OGGPLAY
+copy icys90.skn \Epoc32\Release\wins\UDEB\Z\SYSTEM\apps\OGGPLAY
+pause
