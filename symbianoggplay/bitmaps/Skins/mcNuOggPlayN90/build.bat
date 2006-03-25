ef5d2a543b83bcc3d440448884c2082c4842a356
fig7 <>
2006-03-25 16:45:11 +0000

    Added mcNuOggPlayN90 skin

---
+++ symbianoggplay/bitmaps/Skins/mcNuOggPlayN90/build.bat
@@ -0,0 +1,5 @@
+call bmconv mcNuOggPlayN90.cmd
+call bmconv s60splash.mbm /c16bitmaps/intro.bmp
+// call makesis mcNuOggPlayN90.pkg
+// zip dist/mcNuOggPlayN90.zip -D version.txt mcNuOggPlayN90.mbm mcNuOggPlayN90.skn s60splash.mbm
+pause
---
+++ symbianoggplay/bitmaps/Skins/mcNuOggPlayN90/mcNuOggPlayN90.bat
@@ -0,0 +1,4 @@
+call bmconv mcNuOggPlayN90.cmd
+copy mcNuOggPlayN90.mbm C:\Symbian\8.1a\S60_2nd_FP3\Epoc32\Release\winscw\UDEB\Z\SYSTEM\apps\OGGPLAY
+copy mcNuOggPlayN90.skn C:\Symbian\8.1a\S60_2nd_FP3\Epoc32\Release\winscw\UDEB\Z\SYSTEM\apps\OGGPLAY
+pause
