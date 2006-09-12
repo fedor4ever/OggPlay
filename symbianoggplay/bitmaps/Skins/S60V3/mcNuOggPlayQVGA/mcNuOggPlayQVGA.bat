c7386bf7d5c3b9cfc515bc9609f02ec569f6f4f1
fig7 <>
2006-09-12 01:08:04 +0000

    Update langauge rss files with startup error messages Remove skin batch file (not used)

--- symbianoggplay/bitmaps/Skins/S60V3/mcNuOggPlayQVGA/mcNuOggPlayQVGA.bat
+++
@@ -1,4 +0,0 @@
-call bmconv mcNuOggPlayQVGA.cmd
-copy mcNuOggPlayQVGA.mbm C:\Symbian\8.1a\S60_2nd_FP3\Epoc32\Release\winscw\UDEB\Z\SYSTEM\apps\OGGPLAY
-copy mcNuOggPlayQVGA.skn C:\Symbian\8.1a\S60_2nd_FP3\Epoc32\Release\winscw\UDEB\Z\SYSTEM\apps\OGGPLAY
-pause
